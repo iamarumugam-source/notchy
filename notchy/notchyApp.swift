0896e3ad78613462ba5dcd2c7cc0acc7bf5680cd
Arumugam <nenjunane@gmail.com>
2025-08-30 15:32:46 +0530

    Initial Commit

---
+++ notchy/notchyApp.swift
@@ -0,0 +1,17 @@
+//
+//  notchyApp.swift
+//  notchy
+//
+//  Created by Arjun on 30/08/25.
+//
+
+import SwiftUI
+
+@main
+struct notchyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
